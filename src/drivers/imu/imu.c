bb9f959577817ba28be949f0db2eb73933a094ad
SKubajko <jakubjanus@apollocansat.space>
2026-03-22 21:17:28 +0100

    mpu6050 calibration

--- src/drivers/imu/imu.c
+++ src/drivers/imu/imu.c
@@ -10,6 +10,8 @@
 
 #define ADDR _u(0x68)
 
+int16_t accel_bias[3] = {0}, gyro_bias[3] = {0};
+
 void d_imu_init() {
   LT_T("Initializing MPU6050");
   d_i2c_init();
@@ -24,7 +26,7 @@ void d_imu_init() {
   buf[1] = 0x00;
   d_i2c_write(ADDR, buf, sizeof(buf), false);
   
-  LT_T("Initialized MPU6050");
+  LT_T("Initialized MPU6050. Remember to calibrate.");
   
   vTaskDelay(100 / portTICK_PERIOD_MS);
 }
@@ -48,4 +50,33 @@ void d_imu_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
   val = 0x41;
   d_i2c_write_read(ADDR, &val, sizeof(val), buffer, sizeof(buffer));
   *temp = buffer[0] << 8 | buffer[1];
+}
+
+void d_imu_calibrate() {
+  LT_I("Calibrating MPU6050... Keep it still!");
+  const int samples = 1000;
+  int32_t accel_sum[3] = {0}, gyro_sum[3] = {0};
+  int16_t temp;
+  
+  for (int i = 0; i < samples; i++) {
+    d_imu_read_raw((int16_t*)accel_sum, (int16_t*)gyro_sum, &temp);
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+
+  for (int i = 0; i < 3; i++) {
+    accel_bias[i] = accel_sum[i] / samples;
+    gyro_bias[i] = gyro_sum[i] / samples;
+  }
+
+  LT_I("MPU6050 Calibration done. Accel bias: [%d, %d, %d], Gyro bias: [%d, %d, %d]", 
+      accel_bias[0], accel_bias[1], accel_bias[2],
+      gyro_bias[0], gyro_bias[1], gyro_bias[2]);
+}
+
+void d_imu_read_cal(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
+  d_imu_read_raw(accel, gyro, temp);
+  for (int i = 0; i < 3; i++) {
+    accel[i] -= accel_bias[i];
+    gyro[i] -= gyro_bias[i];
+  }
 }
--- src/drivers/imu/imu.h
+++ src/drivers/imu/imu.h
@@ -4,3 +4,5 @@
 
 void d_imu_init();
 void d_imu_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp);
+void d_imu_calibrate();
+void d_imu_read_cal(int16_t accel[3], int16_t gyro[3], int16_t *temp);
